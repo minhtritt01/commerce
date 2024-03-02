8826ed4abffb78e4e4e69dfc02187b20a799cb87
Phan Tri <tri_phan@onskyhealth.com>
2024-03-02 21:04:37 +0700

    hide greeting

--- src/components/FacebookMsg/FacebookMsg.js
+++ src/components/FacebookMsg/FacebookMsg.js
@@ -5,7 +5,11 @@ export default class FacebookMsg extends Component {
   render() {
     return (
       <FacebookProvider appId='1150283972783743' chatSupport>
-        <CustomChat pageId='197681293439134' minimized={true} />
+        <CustomChat
+          pageId='197681293439134'
+          minimized={true}
+          greetingDialogDisplay='hide'
+        />
       </FacebookProvider>
     );
   }
