d5741b49f6c481c4c755122962b5c1062734946b
Phan Tri <tri_phan@onskyhealth.com>
2024-03-02 12:58:58 +0700

    fix refused display facebook

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,20 @@
-import Router from "next/router";
-import NProgress from "nprogress"; //nprogress module
-import { Provider } from "react-redux";
-import { store } from "../app/store";
-import { Provider as NextAuthProvider } from "next-auth/client";
-import { ToastContainer } from "react-toastify"; //styles of nprogress
-import Layout from "../components/Layout/Layout";
-import "../styles/globals.css";
-import "react-toastify/dist/ReactToastify.css";
-import "nprogress/nprogress.css";
-import { SWRConfig } from "swr";
-import fetcher from "../util/fetch";
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import { Provider } from 'react-redux';
+import { store } from '../app/store';
+import { Provider as NextAuthProvider } from 'next-auth/client';
+import { ToastContainer } from 'react-toastify'; //styles of nprogress
+import Layout from '../components/Layout/Layout';
+import '../styles/globals.css';
+import 'react-toastify/dist/ReactToastify.css';
+import 'nprogress/nprogress.css';
+import { SWRConfig } from 'swr';
+import fetcher from '../util/fetch';
 
 //Binding events.
-Router.events.on("routeChangeStart", () => NProgress.start());
-Router.events.on("routeChangeComplete", () => NProgress.done());
-Router.events.on("routeChangeError", () => NProgress.done());
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -26,6 +26,7 @@ function MyApp({ Component, pageProps }) {
         }}
       >
         <Provider store={store}>
+          <div class='fb-customerchat' page_id='197681293439134'></div>
           <Layout admin={Component?.admin} auth={Component?.auth}>
             <Component {...pageProps} />
             <ToastContainer limit={4} />
