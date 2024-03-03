a9cc09c41d6c7155ceb108ab5628c4aeaafff709
Phan Tri <tri_phan@onskyhealth.com>
2024-03-03 17:51:42 +0700

    remove style

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -26,10 +26,7 @@ function MyApp({ Component, pageProps }) {
         }}
       >
         <Provider store={store}>
-          <div
-            className='fixed z-90 bottom-10 right-8 bg-blue-600 w-20 h-20 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-blue-700 hover:drop-shadow-2xl hover:animate-bounce duration-300'
-            page_id='197681293439134'
-          ></div>
+          <div className='fb-customerchat' page_id='197681293439134'></div>
           <Layout admin={Component?.admin} auth={Component?.auth}>
             <Component {...pageProps} />
             <ToastContainer limit={4} />
